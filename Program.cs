9f1c78a46275e5974adaf2c65406d27dc54afa7a
Jeton245 <44135650+Jeton245@users.noreply.github.com>
2022-09-13 16:54:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[8] { 2, -14, 5, 86, 43, 12, -54, 7 };
+
+            int[] numbers = QuickSort(nums, 0, nums.Length - 1);
+
+            Console.WriteLine($"Sorted array:{string.Join(", ",numbers)}");
+
+            Console.ReadLine();
+        }
+
+
+        static int[] QuickSort(int[] array , int minIndex,int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+            int pivot = FindPivot(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivot-1);
+            QuickSort(array, pivot + 1, maxIndex);
+            return array;
+        }
+
+       static int FindPivot(int[] nums, int minIndex,int maxIndex) 
+        {
+            int pivot = minIndex - 1;
+            int temp = 0;
+         
+
+            for(int i = minIndex; i < maxIndex; i++)
+            {
+                if (nums[i] < nums[maxIndex])
+                {
+                    pivot++;
+                    temp = nums[pivot];
+                    nums[pivot] = nums[i];
+                    nums[i] = temp;
+                    
+                }
+               
+
+            }
+            pivot++;
+            temp = nums[pivot];
+            nums[pivot] = nums[maxIndex];
+            nums[maxIndex] = temp; 
+            return pivot;
+        }
+    }
+}
